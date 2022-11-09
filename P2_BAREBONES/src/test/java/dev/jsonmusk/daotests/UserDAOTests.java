9712243cb13a3cc34c56f71e8e81786bc47d0f31
HikaLamu <lamuhika@gmail.com>
2022-11-09 16:07:33 -0500

    Adding changes

--- P2_BAREBONES/src/test/java/dev/jsonmusk/daotests/UserDAOTests.java
+++ P2_BAREBONES/src/test/java/dev/jsonmusk/daotests/UserDAOTests.java
@@ -45,7 +45,7 @@ public class UserDAOTests {
     // this is kabob case, tests are written in kabob case
     @Test
     @Order(1)
-    void create_employee_test(){
+    void create_user_test(){
         String randomName = "";
         Random random = new Random();
         char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
@@ -53,80 +53,80 @@ public class UserDAOTests {
             int randNum = random.nextInt(26);
             randomName += alphabet[randNum];
         }
-        User newEmployee = new User(0,randomName,"password",true);
-        User savedEmployee = userDAO.createEmployee(newEmployee);
-        System.out.println(savedEmployee);
+        User newUser = new User(0,randomName,"password",true);
+        User savedUser = userDAO.createUser(newUser);
+        System.out.println(savedUser);
 
-        Assertions.assertNotEquals(0,savedEmployee.getId());
+        Assertions.assertNotEquals(0,savedUser.getId());
     }
     @Test
     @Order(2)
-    void get_employee_by_id_test(){
-        User gottenEmployee = userDAO.getEmployeeById(1);
-        System.out.println(gottenEmployee);
-        Assertions.assertNotNull(gottenEmployee);
+    void get_user_by_id_test(){
+        User gottenUser = userDAO.getUserById(1);
+        System.out.println(gottenUser);
+        Assertions.assertNotNull(gottenUser);
     }
     @Test
     @Order(3)
-    void get_employee_by_username_test(){
-        User employee = userDAO.getEmployeeById(1);
-        System.out.println(employee);
+    void get_user_by_username_test(){
+        User user = userDAO.getUserById(1);
+        System.out.println(user);
 
-        String username = employee.getUsername();
-        int id = employee.getId();
+        String username = user.getUsername();
+        int id = user.getId();
 
-        User gottenEmployee = userDAO.getEmployeeByUsername(username);
-        Assertions.assertEquals(id, gottenEmployee.getId());
+        User gottenUser = userDAO.getUserByUsername(username);
+        Assertions.assertEquals(id, gottenUser.getId());
     }
-    @Test
-    @Order(4)
-    void get_all_employees_test(){
-
-        List<User> allEmployees = userDAO.getAllEmployees();
-        for (User e : allEmployees) {
-            System.out.println(e.toString());
-        }
-        Assertions.assertTrue((allEmployees.size()> 0));
-    }
+//    @Test
+//    @Order(4)
+//    void get_all_employees_test(){
+//
+//        List<User> allEmployees = userDAO.getAllEmployees();
+//        for (User e : allEmployees) {
+//            System.out.println(e.toString());
+//        }
+//        Assertions.assertTrue((allEmployees.size()> 0));
+//    }
     @Test
     @Order(5)
-    void update_employee_test(){
-        User employee = userDAO.getEmployeeById(1);
-        User employee2 = new User(employee.getId(),employee.getUsername(),"newPassword",employee.isManager());
-        userDAO.updateEmployee(employee2);
-        User updatedEmployee = userDAO.getEmployeeById(1);
-        Assertions.assertEquals("newPassword",updatedEmployee.getPassword());
+    void update_user_test(){
+        User user = userDAO.getUserById(1);
+        User user2 = new User(user.getId(),user.getUsername(),"newPassword"; /*user.isManager()*/);
+        userDAO.updateUser(user2);
+        User updatedUser = userDAO.getUserById(1);
+        Assertions.assertEquals("newPassword",updatedUser.getPassword());
     }
-    @Test
-    @Order(6)
-    void change_role_test() {
-        User employee = userDAO.getEmployeeByUsername("SomeGuy");
-        boolean firstrole = employee.isManager();
-
-        System.out.println(employee);
-
-        employee = userDAO.changeEmployeeRole(employee, !firstrole);
-
-        System.out.println(employee);
-        Assertions.assertNotEquals(firstrole, employee.isManager());
+//    @Test
+//    @Order(6)
+//    void change_role_test() {
+//        User employee = userDAO.getEmployeeByUsername("SomeGuy");
+//        boolean firstrole = employee.isManager();
+//
+//        System.out.println(employee);
+//
+//        employee = userDAO.changeEmployeeRole(employee, !firstrole);
+//
+//        System.out.println(employee);
+//        Assertions.assertNotEquals(firstrole, employee.isManager());
 
     }
     @Test
     @Order(7)
-    void append_photo_test() {
-        User employee = userDAO.getEmployeeByUsername("SomeGuy");
-        System.out.println(employee);
+    void upload_photo_test() {
+        User user = userDAO.getUserByUsername("SomeGuy");
+        System.out.println(user);
         byte[] bytes = new byte[50];
-        userDAO.appendPhotoToEmployee(employee, bytes);
-        User employee2 = userDAO.getEmployeeByUsername("SomeGuy");
-        System.out.println(employee2);
-        byte[] bytes2 =  employee2.getPhoto();
+        userDAO.uploadPhotoToUser(user, bytes);
+        User user2 = userDAO.getUserByUsername("SomeGuy");
+        System.out.println(user2);
+        byte[] bytes2 =  user2.getPhoto();
         Assertions.assertEquals(50, bytes2.length);
     }
     @Test
     @Order(8)
-    void delete_employee_by_id_test(){
-        boolean result = userDAO.deleteEmployeeById(8);
+    void delete_user_by_id_test(){
+        boolean result = userDAO.deleteUserById(8);
         Assertions.assertTrue(result);
     }
 
